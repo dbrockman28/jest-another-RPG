c8169fd27ade0da75d64fce4180cd7f4afdece00
Rowdy Goat <dbrockman28@gmail.com>
2020-11-30 21:13:19 -0600

    added inventory w/ 2 initial potions; mock potion added

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -1,5 +1,8 @@
-const { test, expect } = require("@jest/globals");
+const Potion = require("../lib/Potion");
+jest.mock("../lib/Potion");
+
 const Player = require("../lib/Player");
+const { expect } = require("@jest/globals");
 
 test("creates a player object", () => {
   const player = new Player("Dave");
@@ -8,4 +11,5 @@ test("creates a player object", () => {
   expect(player.health).toEqual(expect.any(Number));
   expect(player.strength).toEqual(expect.any(Number));
   expect(player.agility).toEqual(expect.any(Number));
+  expect(player.inventory).toEqual(expect.arrayContaining([expect.any(Object)]))
 });
